250af50cf7c35bd88d130a20e18fbc4b16c86efb
ceren <cereyild@icloud.com>
2024-07-20 23:39:47 +0200

    question numbers corrected

--- js/questions.js
+++ js/questions.js
@@ -18,43 +18,43 @@ var questions = [
         correctAnswer: 3
     },
     {
-        question: "Question 3: Which country's flag is this?",
+        question: "Question 4: Which country's flag is this?",
         image: "img/flag4.png",
         answers: ["Sweden", "Israel", "Denmark", "Bulgaria"],
         correctAnswer: 0
     },
     {
-        question: "Question 3: Which country's flag is this?",
+        question: "Question 5: Which country's flag is this?",
         image: "img/flag5.png",
         answers: ["Angola", "Russia", "Gambia", "Italy"],
         correctAnswer: 3
     },
     {
-        question: "Question 3: Which country's flag is this?",
+        question: "Question 6: Which country's flag is this?",
         image: "img/flag6.png",
         answers: ["Belgium", "Russia", "China", "Kurdistan"],
         correctAnswer: 1
     },
     {
-        question: "Question 3: Which country's flag is this?",
+        question: "Question 7: Which country's flag is this?",
         image: "img/flag7.png",
         answers: ["Vietnam", "Sudan", "Spain", "Thailand"],
         correctAnswer: 2
     },
     {
-        question: "Question 3: Which country's flag is this?",
+        question: "Question 8: Which country's flag is this?",
         image: "img/flag8.png",
         answers: ["Uruguay", "Argentina", "Albania", "Bahamas"],
         correctAnswer: 0
     },
     {
-        question: "Question 3: Which country's flag is this?",
+        question: "Question 9: Which country's flag is this?",
         image: "img/flag9.png",
         answers: ["Eritrea", "Canada", "Cambodia", "Mexico"],
         correctAnswer: 3
     },
     {
-        question: "Question 3: Which country's flag is this?",
+        question: "Question 10: Which country's flag is this?",
         image: "img/flag10.png",
         answers: ["Kurdistan", "Albania", "Fiji", "Estonia"],
         correctAnswer: 1
